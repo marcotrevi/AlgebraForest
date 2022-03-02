a8a7c0c710281b82a39417e6ab8534e3917246dc
Marco <marco.trevi@gmail.com>
2022-03-02 17:38:02 +0100

    added networkx help

--- script.py
+++ script.py
@@ -1,6 +1,43 @@
+from datetime import timedelta
+from random import *
 import sympy
 from sympy import *
-x,y = symbols("x y")
-expr1 = cos(x)
-expr2 = sin(x)
-print(expr1.subs(x,expr2).subs(x,0))
+import utilities as U
+import networkx as nx
+import matplotlib.pyplot as plt
+
+#from random import choice, randint
+#from sympy import FunctionClass, Add, Mul, cos, sin, binomial, arity
+
+def treeToExpression(tree):
+    # gets a tree graph in networkx and returns a sympy expression
+    expr = tree
+    return expr
+
+n = 6
+
+print('start')
+T = nx.nonisomorphic_trees(n, create="graph")
+print('stop')
+
+g = list(T)
+k = 4
+G = g[k]
+# G is a tree graph
+
+pos = nx.spring_layout(G)
+
+# dictionary comprehension
+labels = {x:x for x in range(n)}
+root = 5
+
+T = nx.bfs_tree(G,root)
+# builds a digraph (BFS tree)
+# now neighbors of a node are only children
+
+
+
+nx.draw_networkx_nodes(T, pos, node_size=500)
+nx.draw_networkx_edges(T, pos, edgelist=T.edges(), edge_color='black')
+nx.draw_networkx_labels(T, pos, labels, font_color='white')
+plt.show()
---
+++ utilities.py
@@ -0,0 +1,16 @@
+def sumExpr(x,y):
+    return x+y
+
+def prodExpr(x,y):
+    return x*y
+
+def powExpr(x,y):
+    return x**y
+
+def buildExpr(type, x ,y):
+    if(type == 1):
+        return x+y
+    elif(type == 2):
+        return x*y
+
+
